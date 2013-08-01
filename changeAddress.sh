8c841776a61b8bd3f5a2348f6af3f3bb8a1ab408
Julian Tosh <julian@tosh.us>
2013-08-01 08:50:54 -0700

    changeAddress needs to connect to server and retrieve latest balance to properly identify next available bitcoin address

--- changeAddress.sh
+++ changeAddress.sh
@@ -7,6 +7,8 @@ fi
 
 walletFile=$1
 
+electrum -w $walletFile getbalance
+
 electrum -w $walletFile -b listaddresses | grep : | sed 's/^.*: //; s/[\",]//g;' | paste - - | grep "\b0$" | head -n 1 | awk '{print $1}' >> $HOME/.mineMonitor/bitcoin.addr
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Updated payout address to $(tac $HOME/.mineMonitor/bitcoin.addr | head -n 1). Restarting bfgminer." >> $HOME/mineMonitor.log
